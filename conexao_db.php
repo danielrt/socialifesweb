2ace73c0a2b09a8bd84da6029dca66c87dd940a2
danielrt <danielrtrindade1@gmail.com>
2025-02-13 19:06:59 -0300

    Update conexao_db.php

--- conexao_db.php
+++ conexao_db.php
@@ -2,20 +2,12 @@
 
 // Abre uma conexao com o BD.
 
-$host        = "host = pg-32571217-dispmoveisbsi-a778.d.aivencloud.com;";
-$port        = "port = 17551;";
-$dbname      = "dbname = socialifes;";
-$dbuser 	 = "avnadmin";
-$dbpassword	 = "AVNS_BUR3AJuWsgm2Y6ipXBB";
-
-
-
-// dados de conexao com o b4app. Usar somente caso esteja usando b4app
-//$host        = "host = " . getenv("BD_HOST") . ";";
-//$port        = "port = " . getenv("BD_PORT") . ";";
-//$dbname      = "dbname = " . getenv("BD_DATABASE") . ";";
-//$dbuser 	 = getenv("BD_USER");
-//$dbpassword	 = getenv("BD_PASSWORD");
+// dados de conexao com o b4app
+$host        = "host = " . getenv("BD_HOST") . ";";
+$port        = "port = " . getenv("BD_PORT") . ";";
+$dbname      = "dbname = " . getenv("BD_DATABASE") . ";";
+$dbuser 	 = getenv("BD_USER");
+$dbpassword	 = getenv("BD_PASSWORD");
 
 // para conectar ao mysql, substitua pgsql por mysql
 $db_con= new PDO('pgsql:' . $host . $port . $dbname, $dbuser, $dbpassword);
